2073966c3bc0d5775af9fa064cfdfdcb3e839a9d
Camila Pessoa <caape13@hotmail.com>
2022-08-24 11:48:50 +0100

    add test folder and file

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const books = require('./api.js');
+const books = require('./api.js'); //API containing books
 
 app.use(cors())
 app.use(express.json())
