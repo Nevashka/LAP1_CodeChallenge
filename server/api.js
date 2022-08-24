5b3bd821223842d59652ebb22779f6836ad49c10
Camila Pessoa <caape13@hotmail.com>
2022-08-24 11:41:05 +0100

    add api.js file with books array and call it at app.js

---
+++ server/api.js
@@ -0,0 +1,14 @@
+const books = [
+    {book: 'The Black Swan: The Impact of the Highly Improbable', author: 'Nassim Nicholas Taleb'},
+    {book: 'The Second Sex', author: 'Simone de Beauvoir'},
+    {book: 'Chatter. The voice in Our Head and How to Harness it', author: 'Ethan Kross'},
+    {book: 'Persepolis', author: 'Marjane Satrapi'},
+    {book: 'Sapiens: A Brief History of Humankind', author: 'Yuval Noah Harari'},
+    {book: 'Utopia for Realists and How We can get There', author: 'Rutger Bregman'},
+    {book: "The Drunkard's Walk: How Randomness Rules Our Live", author: 'Leonard Mlodinow'},
+    {book: 'Thinking, Fast and Slow', author:'Daniel Kahneman'},
+    {book: 'Dune', author: 'Frank Herbert'},
+    {book: 'The Sacred and The Profane: The Nature of Religion', author: 'Mircea Eliade'}
+]
+
+module.exports = {books};
