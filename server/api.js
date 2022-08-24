fffc56ce9e9ef7b9808f18a9a3beca187456f103
Camila Pessoa <caape13@hotmail.com>
2022-08-24 13:37:02 +0100

    change quotes

--- server/api.js
+++ server/api.js
@@ -1,14 +1,14 @@
 const books = [
-    {book: 'The Black Swan: The Impact of the Highly Improbable', author: 'Nassim Nicholas Taleb'},
-    {book: 'The Second Sex', author: 'Simone de Beauvoir'},
-    {book: 'Chatter. The voice in Our Head and How to Harness it', author: 'Ethan Kross'},
-    {book: 'Persepolis', author: 'Marjane Satrapi'},
-    {book: 'Sapiens: A Brief History of Humankind', author: 'Yuval Noah Harari'},
-    {book: 'Utopia for Realists and How We can get There', author: 'Rutger Bregman'},
-    {book: "The Drunkard's Walk: How Randomness Rules Our Live", author: 'Leonard Mlodinow'},
-    {book: 'Thinking, Fast and Slow', author:'Daniel Kahneman'},
-    {book: 'Dune', author: 'Frank Herbert'},
-    {book: 'The Sacred and The Profane: The Nature of Religion', author: 'Mircea Eliade'}
+    {id:"1", book: "The Black Swan: The Impact of the Highly Improbable", author: "Nassim Nicholas Taleb"},
+    {id:"2", book: "The Second Sex", author: "Simone de Beauvoir"},
+    {id:"3", book: "Chatter. The voice in Our Head and How to Harness it", author: "Ethan Kross"},
+    {id:"4", book: "Persepolis", author: "Marjane Satrapi"},
+    {id:"5",book: "Sapiens: A Brief History of Humankind", author: "Yuval Noah Harari"},
+    {id:"6",book: "Utopia for Realists and How We can get There", author: "Rutger Bregman"},
+    {id:"7", book: "The Drunkard's Walk: How Randomness Rules Our Live", author: "Leonard Mlodinow"},
+    {id:"8", book: "Thinking, Fast and Slow", author:"Daniel Kahneman"},
+    {id:"9", book: "Dune", author: "Frank Herbert"},
+    {id:"10", book: "The Sacred and The Profane: The Nature of Religion", author: "Mircea Eliade"}
 ]
 
 module.exports = {books};
