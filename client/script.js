19caebda3f10dcc3aacaf8ee1ef0c26ee261b278
mike-rv <mikerv73@gmail.com>
2022-08-25 11:35:21 +0100

    modify app.js api.js>api.json result.html script.js

--- client/script.js
+++ client/script.js
@@ -1,16 +1,6 @@
-let searchBtn = document.querySelector('.button-search')
-searchBtn.addEventListener('click', fetcher);
-
-function fetcher(e) {
-  e.preventDefault()
-  fetch('http://localhost:3000/books')
-    .then((response) => response.json()) // promise
-    .then((data) => {
-      console.log(data)
-      window.location.assign("result.html")
-    });
-}
-// .replace(/{"quote":/, "")
-//   .replace("}", "").replaceAll("\"", "")
-
-// document.querySelector("#output").textContent = JSON.stringify(data[Math.floor(Math.random() * data.length)])
+fetch('http://localhost:3000/books')
+.then((response) => response.json()) // promise
+.then((data) => {
+  console.log(data)
+  document.querySelector("#text").textContent = JSON.stringify(data)
+});
--- server/app.js
+++ server/app.js
@@ -1,28 +1,13 @@
 const express = require('express');
+const books = require('./api.json')
 const app = express();
 const cors = require('cors');
-// const router = express.Router();
-
-// const books = require('./api.js'); //API containing books
 
 app.use(cors())
 app.use(express.json())
 
-const books = [
-    {id:"1", book: "The Black Swan: The Impact of the Highly Improbable", author: "Nassim Nicholas Taleb"},
-    {id:"2", book: "The Second Sex", author: "Simone de Beauvoir"},
-    {id:"3", book: "Chatter. The voice in Our Head and How to Harness it", author: "Ethan Kross"},
-    {id:"4", book: "Persepolis", author: "Marjane Satrapi"},
-    {id:"5",book: "Sapiens: A Brief History of Humankind", author: "Yuval Noah Harari"},
-    {id:"6",book: "Utopia for Realists and How We can get There", author: "Rutger Bregman"},
-    {id:"7", book: "The Drunkard's Walk: How Randomness Rules Our Live", author: "Leonard Mlodinow"},
-    {id:"8", book: "Thinking, Fast and Slow", author:"Daniel Kahneman"},
-    {id:"9", book: "Dune", author: "Frank Herbert"},
-    {id:"10", book: "The Sacred and The Profane: The Nature of Religion", author: "Mircea Eliade"}
-]
-
-function getRandomBook () {
-    let book = Math.floor(Math.random()*10);
+function getRandomBook() {
+    let book = Math.floor(Math.random() * 10);
     return books[book]
 }
 
@@ -34,18 +19,15 @@ app.get('/books', (req, res) => {
     res.send(books);
 })
 
+app.get('/random-book', (req, res) => {
+    res.send(getRandomBook(books));
+})
+
 app.get('/books/:id', (req, res) => {
-
-     try {
-
-        res.send(books[req.params.id])
-
-    } catch(err) {
-''
-        res.json(err);
-        res.status(404).send(err);
-
-      }
+    if (req.params.id < 0 || req.params.id > 9) {
+        throw Error('book id: ' + req.params.id + ' does not exist')
+    }
+    res.send(books[req.params.id])
 })
 
 module.exports = app;
